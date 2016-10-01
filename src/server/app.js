b2d8ec3e9affadf0a3d2a97da3d72e0db5568a24
Shane Dublin <shanedublin27@gmail.com>
2016-09-30 19:39:47 -0700

    hey listen!!

--- src/server/app.js
+++ src/server/app.js
@@ -2,7 +2,12 @@ var express = require('express');
 var app = express();
 var config = require('./config/config.js');
 
-app.set('port',3001);
+//app.set('port',3001);
 
 app.use(express.static(config.publicFolder));
 
+console.log('app init');
+
+app.listen(3002,function(){
+	console.log('hello!');
+});
